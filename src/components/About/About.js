d8de704accd4b187dbd3f9386c314b99008d67ea
wolv66 <wolv66@users.noreply.github.com>
2020-04-07 13:10:34 -0700

    made more responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,7 +16,7 @@ export default function About() {
                 <h1>About Me and my WebSite</h1>
                 <p>Hi! My name is Paul and I'm FrontEnd Developer! 
                     I was born in Russia, and 2 years ago my family moved to Canada and now we are enjoing this beautiful country.
-                    I started my карьера 1 year ago as Junior FronEnd Developer in EKit company. 
+                    I started my career 1 year ago as Junior FronEnd Developer in EKit company. 
                     On that moment they were looking for someone who can help them with their ideas and who can learn really fast. And they took me! For the last year 
                     I learned a lot of interesting things such as - React! Yes, I fell in love with this framework (or library) and now everyday I'm enjoing to learn something
                     more about it - Redux, Hooks. Every day I'm trying to upgrade my skills and do something new.
