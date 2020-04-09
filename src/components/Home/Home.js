7eade8aec608055f97712a286c77b4b93a2d16ce
wolv66 <wolv66@users.noreply.github.com>
2020-04-09 15:30:05 -0700

    name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = (props) => {
         <div className='grid-box'>
             <div className='box1'>
                 <div className="card-text">
-                    <h1>Paul </h1>
+                    <h1>P </h1>
                     <h1>Zakharov</h1>
 
                     <h3>FrontEnd Developer</h3>
